6c337c546ca16790acc990e5f6ffe7b498334b66
Dario Ceballos <dario_tec_aut@hotmail.com.ar>
2022-09-05 17:53:03 -0300

    Listado - control de errores

--- src/components/Listado.js
+++ src/components/Listado.js
@@ -1,6 +1,7 @@
 import { useEffect, useState } from 'react';
 import axios from 'axios';
 import { Link, Navigate } from 'react-router-dom';
+import swAlert from '@sweetalert/with-react';
 import './Listado.css';
 
 function Listado() {
@@ -14,9 +15,12 @@ function Listado() {
       .then(response => {
         const apiData = response.data;
         setMovieList(apiData.results);
+      }).catch( () => {
+        swAlert(<h2>Error de carga intenta mas tarde</h2>)
       })
   }, [setMovieList]);
   console.log(moviesList);
+
 
 
   return (
@@ -31,7 +35,7 @@ function Listado() {
                 <img className="card-img-top" src={`https://image.tmdb.org/t/p/w500/${oneMovie.poster_path}`} alt="Card image cap" />
                 <div className="card-body">
                   <h5 className="card-title">{oneMovie.title}</h5>
-                  <p className="card-text">{oneMovie.overview.substring(0,100)}</p>
+                  <p className="card-text">{oneMovie.overview.substring(0,100)}...</p>
                   <Link to="#" className="btn btn-primary">Go somewhere</Link>
                 </div>
               </div>
